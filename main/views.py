5b716a80913024a7d002778dede9d1dea4c84517
Medved <eevalushionip@gmail.com>
2024-11-18 20:27:21 +0600

    import model views

--- main/views.py
+++ main/views.py
@@ -5,11 +5,15 @@ from django.views.generic import ListView
 
 def index_viu(request):
     hotel_room = HotelRoom.objects.all().exclude(status=1).order_by('-id')[:3]
-
+    services = Services.objects.all()
 
     return render(
         request=request,
-        template_name='main/index.html'
+        template_name='main/index.html',
+        context={
+            "hotel_room":hotel_room,
+            "services":services
+        }
     )
 
 def test(request):
